4699e7ae2eb6fd037406a20239753917854ff9c8
botr1k <a.lavnikevich@gmail.com>
2024-12-25 15:56:18 +0300

    hw-08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            if (action.payload === 'up') {
+                return [...state.sort((a,b) => a.name.localeCompare(b.name, 'ru'))]
+            } else {
+                return [...state.sort((a,b) => b.name.localeCompare(a.name, 'ru'))]
+            }
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state.filter(user => user.age >= action.payload)]
         }
         default:
             return state
     }
 }
+
