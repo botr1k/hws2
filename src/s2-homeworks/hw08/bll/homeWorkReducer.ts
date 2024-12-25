9cf80f5c4a5ee6e16d6aec7e57ddf0073f518b91
botr1k <a.lavnikevich@gmail.com>
2024-12-25 16:18:34 +0300

    fix reducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,21 +1,21 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+export const homeWorkReducer = (state: UserType[] , action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': {
             if (action.payload === 'up') {
-                return [...state.sort((a,b) => a.name.localeCompare(b.name, 'ru'))]
+                return [...state].sort((a, b) => a.name.localeCompare(b.name, 'ru'))
             } else {
-                return [...state.sort((a,b) => b.name.localeCompare(a.name, 'ru'))]
+                return [...state].sort((a, b) => b.name.localeCompare(a.name, 'ru'))
             }
         }
         case 'check': {
 
-            return [...state.filter(user => user.age >= action.payload)]
+            return state.filter(user => user.age >= action.payload)
         }
         default:
             return state
