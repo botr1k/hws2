a5b8e41eb1c204080a4669b5ea7aa1d6968b0714
botr1k <a.lavnikevich@gmail.com>
2025-01-02 19:47:33 +0300

    HW 09

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -10,76 +10,99 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
-        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
-        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
 
+        if (timerId !== undefined) return;
+
+        const id = setInterval(() => {
+            setDate(new Date())
+        }, 1000) as unknown as number
+
+        setTimerId(id)
     }
 
     const stop = () => {
-        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
+
+        if (timerId === undefined) return;
+
+        clearInterval(timerId);
+        setTimerId(undefined);
 
     }
 
-    const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
+    const onMouseEnter = () => {
+        setShow(true)
     }
-    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+    const onMouseLeave = () => {
+        setShow(false)
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = date.toLocaleTimeString('en-Us', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+    }) || '' // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+
+    const stringDate = date.toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    }) || ''// день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringDay = date.toLocaleString('en-Us', {
+        weekday: 'long'
+    }) || ''  // пишут студенты
+    const stringMonth = date.toLocaleString('en-Us', {
+        month: 'long'
+    }) || ''  // пишут студенты
 
     return (
-        <div className={s.clock}>
-            <div
-                id={'hw9-watch'}
-                className={s.watch}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                <span id={'hw9-day'}>{stringDay}</span>,{' '}
-                <span id={'hw9-time'}>
+      <div className={s.clock}>
+          <div
+            id={'hw9-watch'}
+            className={s.watch}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+              <span id={'hw9-day'}>{stringDay}</span>,{' '}
+              <span id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
-            </div>
+          </div>
 
-            <div id={'hw9-more'}>
-                <div className={s.more}>
-                    {show ? (
-                        <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
-                        </>
-                    ) : (
-                        <>
-                            <br/>
-                        </>
-                    )}
-                </div>
-            </div>
+          <div id={'hw9-more'}>
+              <div className={s.more}>
+                  {show ? (
+                    <>
+                        <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+                        <span id={'hw9-date'}>{stringDate}</span>
+                    </>
+                  ) : (
+                    <>
+                        <br/>
+                    </>
+                  )}
+              </div>
+          </div>
 
-            <div className={s.buttonsContainer}>
-                <SuperButton
-                    id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
-                    onClick={start}
-                >
-                    start
-                </SuperButton>
-                <SuperButton
-                    id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
-                    onClick={stop}
-                >
-                    stop
-                </SuperButton>
-            </div>
-        </div>
+          <div className={s.buttonsContainer}>
+              <SuperButton
+                id={'hw9-button-start'}
+                disabled={timerId !== undefined} // пишут студенты // задизэйблить если таймер запущен
+                onClick={start}
+              >
+                  start
+              </SuperButton>
+              <SuperButton
+                id={'hw9-button-stop'}
+                disabled={timerId === undefined} // пишут студенты // задизэйблить если таймер не запущен
+                onClick={stop}
+              >
+                  stop
+              </SuperButton>
+          </div>
+      </div>
     )
 }
 
